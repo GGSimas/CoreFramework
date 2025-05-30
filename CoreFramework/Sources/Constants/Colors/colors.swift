22cf6bf4da31224f8575e3d63e8a59b67ba825cf
Gabriel Simões <simoes.gabrielm@gmail.com>
2025-05-30 12:08:40 -0300

    inicio do projeto

---
+++ CoreFramework/Sources/Constants/Colors/colors.swift
@@ -0,0 +1,24 @@
+//
+//  colors.swift
+//  reminder
+//
+//  Created by Gabriel Simões on 07/05/25.
+//
+
+import Foundation
+import UIKit
+
+public struct Colors {
+    static let red_base = UIColor(red: 192/255, green: 38/255, blue: 54/255, alpha: 1)
+    static let red_light = UIColor(red: 235/255, green: 60/255, blue: 220/255, alpha: 1)
+    static let blue_base = UIColor(red: 51/255, green: 79/255, blue: 220/255, alpha: 1)
+    static let blue_light = UIColor(red: 110/255, green: 131/255, blue: 236/255, alpha: 1)
+    static let gray_100 = UIColor(red: 23/255, green: 34/255, blue: 43/255, alpha: 1)
+    static let gray_200 = UIColor(red: 41/255, green: 60/255, blue: 76/255, alpha: 1)
+    static let gray_300 = UIColor(red: 77/255, green: 112/255, blue: 143/255, alpha: 1)
+    static let gray_400 = UIColor(red: 162/255, green: 185/255, blue: 205/255, alpha: 1)
+    static let gray_500 = UIColor(red: 202/255, green: 215/255, blue: 226/255, alpha: 1)
+    static let gray_600 = UIColor(red: 215/255, green: 225/255, blue: 234/255, alpha: 1)
+    static let gray_700 = UIColor(red: 232/255, green: 238/255, blue: 243/255, alpha: 1)
+    static let gray_800 = UIColor(red: 248/255, green: 250/255, blue: 251/255, alpha: 1)
+}
---
+++ CoreFramework/Sources/Constants/Metrics/metrics.swift
@@ -0,0 +1,26 @@
+//
+//  metrics.swift
+//  reminder
+//
+//  Created by Gabriel Simões on 07/05/25.
+//
+
+import Foundation
+import UIKit
+
+public struct Metrics {
+    public static let huge: CGFloat = 48
+    public static let high: CGFloat = 32
+    public static let medium: CGFloat = 24
+    public static let medium_tiny: CGFloat = 16
+    public static let small: CGFloat = 12
+    public static let small_tiny: CGFloat = 10
+    public static let tiny: CGFloat = 8
+    public static let little: CGFloat = 4
+    
+    public static let buttonSize: CGFloat = 56
+    public static let buttonSizeHigh: CGFloat = 40
+    public static let textInputSize: CGFloat = 56
+    public static let profileImageSize: CGFloat = 68
+    public static let profileBackgroundSize: CGFloat = 240
+}
